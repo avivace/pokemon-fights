e11992c9446074a5f30bcb5ef425ee6f0df63cc2
marcoBelotti <m.belotti20@campus.unimib.it>
2018-05-31 16:21:07 +0200

    Script aggiornanto con metodo DT

--- script.R
+++ script.R
@@ -11,33 +11,57 @@ library(rpart.plot)
 library(ROCR)
 library(magrittr)
 
+# Importazione Dataset
+
 # Importo il file csv con l'elenco di tutti i Pokemon e relative caratteristiche
 pokemon<-read.csv("./pokemon.csv",sep=",",stringsAsFactors=F)
 
-# Attribuisco un nome a parametri
-colnames(pokemon)<-c("id","Name","Type.1","Type.2","HP","Attack","Defense","Sp.Atk","Sp.Def","Speed","Generation","Legendary")
-
-Type.1<-c("Dragon","Steel","Flying","Psychic","Rock" ,"Fire","Electric" ,"Dark","Ghost" ,"Ground","Ice", "Water","Grass","Fighting", "Fairy" ,"Poison","Normal","Bug")
-#color<-c("#6F35FC","#B7B7CE","#A98FF3","#F95587","#B6A136","#EE8130","#F7D02C","#705746","#735797","#E2BF65","#96D9D6","#6390F0","#7AC74C","#C22E28","#D685AD","#A33EA1","#A8A77A","#A6B91A")
-
-# names contiene l'associazione id-nome del pokemon
-names <- pokemon %>% select(id, Name)
-
-# Modeling
-
-# Rileggo il file con i combattimenti e risultati per fare il training del modello
+# Importo il file csv con i combattimenti e i risultati per fare il training ed il testing del modello
 combats<-read.csv('./combats.csv',sep=",",stringsAsFactors=F)
 
-# Trova i nomi dei contendenti dati gli ID
+# Importo il file con i combattimenti privi di risultati per mostrare un esempio di applicazione "Reale" del modello di ML
+real_test<-read.csv('./tests.csv',sep=",",stringsAsFactors=F)
+
+# Importo il file csv contenente informazioni riguardo le relazioni tra tipi di pokemon in merito alla proprietà di attacco
+# Meglio sostituirlo con un csv
+atk<-c('Normal','Fire','Water','Electric','Grass','Ice','Fighting','Poison','Ground','Flying','Psychic','Bug','Rock','Ghost','Dragon','Dark','Steel','Fairy')
+normal<-c(1,1,1,1,1,1,2,1,1,1,1,1,1,0,1,1,1,1)
+fire<-c(1,0.5,2,1,0.5,0.5,1,1,2,1,1,0.5,2,1,1,1,0.5,0.5)
+water<-c(1,0.5,0.5,2,2,0.5,1,1,1,1,1,1,1,1,1,1,0.5,1)
+elec<-c(1,1,1,0.5,1,1,1,1,2,0.5,1,1,1,1,1,1,0.5,1)
+grass<-c(1,2,0.5,0.5,0.5,2,1,2,0.5,2,1,2,1,1,1,1,1,1)
+ice<-c(1,2,1,1,1,0.5,2,1,1,1,1,1,2,1,1,1,2,1)
+fighting<-c(1,1,1,1,1,1,1,1,1,2,2,0.5,0.5,1,1,0.5,1,2)
+poison<-c(1,1,1,1,0.5,1,0.5,0.5,2,1,2,0.5,1,1,1,1,1,0.5)
+ground<-c(1,1,2,0,2,2,1,0.5,1,1,1,1,0.5,1,1,1,1,1)
+flying<-c(1,1,1,2,0.5,2,0.5,1,0,1,1,0.5,2,1,1,1,1,1)
+psychic<-c(1,1,1,1,1,1,0.5,1,1,1,0.5,2,1,2,1,2,1,1)
+bug<-c(1,2,1,1,0.5,1,0.5,1,0.5,2,1,1,2,1,1,1,1,1)
+rock<-c(0.5,0.5,2,1,2,1,2,0.5,2,0.5,1,1,1,1,1,1,2,1)
+ghost<-c(0,1,1,1,1,1,0,0.5,1,1,1,0.5,1,2,1,2,1,1)
+dragon<-c(1,0.5,0.5,0.5,0.5,2,1,1,1,1,1,1,1,1,2,1,1,2)
+dark<-c(1,1,1,1,1,1,2,1,1,1,0,2,1,0.5,1,0.5,1,2)
+steel<-c(0.5,2,1,1,0.5,0.5,2,0,2,0.5,0.5,0.5,0.5,1,0.5,1,0.5,0.5)
+fairy<-c(1,1,1,1,1,1,0.5,2,1,1,1,0.5,1,1,0,0.5,2,1)
+
+mytable<-data.frame(Attacking=atk,Normal=normal,Fire=fire,Water=water,Electric=elec,Grass=grass,Ice=ice,Fighting=fighting,Poison=poison,Ground=ground,Flying=flying,Psychic=psychic,Bug=bug,Rock=rock,Ghost=ghost,Dragon=dragon,Dark=dark,Steel=steel,Fairy=fairy)
+
+# Definisco il nome delle colonne di pokemon
+colnames(pokemon)<-c("id","Name","Type.1","Type.2","HP","Attack","Defense","Sp.Atk","Sp.Def","Speed","Generation","Legendary")
+
+# Definisco l'associazione id-nome del pokemon
+names <- pokemon %>% select(id, Name)
+
+# Integrazione dei dati provenienti dai diversi Dataset
+
+# Integro in combats il nome dei pokemon avversari ricercandoli tramite il loro id
 combats$First_pokemon_name<-sapply(combats$First_pokemon, function(x) names$Name[match(x, names$id)])
 combats$Second_pokemon_name<-sapply(combats$Second_pokemon, function(x) names$Name[match(x, names$id)])
 
-# Vengono recuperati i parametri dei contendenti e calcolate le differenze su alcuni di essi
+# Integro in combats i vari parametri caratteristici dei due pockemon avversari e calcolo le differenze tra di essi
 combats$First_pokemon_attack<-sapply(combats$First_pokemon_name, function(x) pokemon$Attack[match(x, pokemon$Name)])
 combats$Second_pokemon_attack<-sapply(combats$Second_pokemon_name, function(x) pokemon$Attack[match(x, pokemon$Name)])
 combats$Diff_attack<-combats$First_pokemon_attack - combats$Second_pokemon_attack
-
-combats$winner_first_label<-ifelse(combats$Winner==combats$First_pokemon,'yes','no')
 
 combats$First_pokemon_defense<-sapply(combats$First_pokemon_name, function(x) pokemon$Defense[match(x, pokemon$Name)])
 combats$Second_pokemon_defense<-sapply(combats$Second_pokemon_name, function(x) pokemon$Defense[match(x, pokemon$Name)])
@@ -64,42 +88,85 @@ combats$Second_pokemon_type<-sapply(combats$Second_pokemon_name, function(x) pok
 combats$First_pokemon_legendary<-sapply(combats$First_pokemon_name, function(x) pokemon$Legendary[match(x, pokemon$Name)])
 combats$Second_pokemon_legendary<-sapply(combats$Second_pokemon_name, function(x) pokemon$Legendary[match(x, pokemon$Name)])
 
+# Determino attributo winner_first_label per la tabella combats
+combats$winner_first_label<-ifelse(combats$Winner==combats$First_pokemon,'yes','no')
+
+# Funzione utilizzata per ottenere informazione sulla relazione tra due pokemon avversari (informazione contenuta in mytable)
+makeAdvantage2<-function(type_1,type_2){
+  val <- mytable[ which(mytable$Attacking==type_1),c(type_2)]
+  if(val==0){
+    return('no effect')
+  }
+  else if(val==0.5){
+    return('not too effective')
+  }
+  else if(val==1){
+    return('normal')
+  }
+  else if(val==2){
+    return('effective')
+  }
+}
+
+# Determino attributo advantage per la tabella combats
+combats$advantage<-mapply(makeAdvantage2, combats$First_pokemon_type, combats$Second_pokemon_type)
+
 # salvo combats su csv
 
-#scale numerical features
-temp<- data.frame(combats %>% dplyr::select(winner_first_label,Diff_attack ,Diff_defense, Diff_sp_defense,Diff_sp_attack,Diff_speed ,Diff_HP, First_pokemon_legendary, Second_pokemon_legendary))
-# Determino quali colonne di temp sono attributi di tipo numerico
+# Mostro distrubuzione percentuale sulla base di advantage in relazione al fatto di essere il pokemon che attacca per primo
+combats %>% 
+  dplyr::select(advantage, winner_first_label) %>% 
+  dplyr::group_by(advantage,winner_first_label) %>% 
+  dplyr::summarize(count=n()) %>% 
+  dplyr::group_by(advantage) %>% 
+  dplyr::mutate(perc = count / sum(count))
+
+temp <- data.frame(combats %>% dplyr::select(winner_first_label,Diff_attack ,Diff_defense, Diff_sp_defense,Diff_sp_attack,Diff_speed ,Diff_HP, First_pokemon_legendary, Second_pokemon_legendary, advantage))
 ind <- sapply(temp, is.numeric)
-# temp[ind] contiene solo le colonne di combats aventi valori di tipo numerico
-temp[ind] <- lapply(temp[ind], scale) # Scala le colonne
+temp[ind] <- lapply(temp[ind], scale)
 
 # Garanzia di riproduzione dei risultati
-set.seed(1234)
+set.seed(2345)
 
-# Partiziono tra training e testing
+# Suddivido tra Training e Testing
 split <- createDataPartition(y=temp$winner_first_label, p = 0.75, list = FALSE)
 train <- temp[split,]
 test <- temp[-split,]
 
-trControl <- trainControl(method = "cv",number = 10)
+# Training del modello
+res.tree<-train(winner_first_label~Diff_attack+Diff_defense+Diff_sp_defense+Diff_sp_attack+Diff_speed+Diff_HP+First_pokemon_legendary+Second_pokemon_legendary+advantage,data=train,method='rpart',trControl = trainControl(method = "cv",number = 10))
 
-res<-train(winner_first_label~.,data=train,method='svmLinear',trControl = trControl,metric='Accuracy') # la metrica per scegliere il modello migliore si basa sull'accuratezza
+# Testing del Modello
+probs <- predict(res.tree, newdata=test, type='prob')
 
-cm <- caret::confusionMatrix(res)
+# Aggiungo a prob l'attributo contenente le informazione sul vincitore (Winner_first_label)
+probs<-data.frame(cbind(probs,winner_first = test$winner_first_label))
+# Aggiungo attributo winner_first_num pari 0 se il primo pokemon ad attaccare non vince, 1 altrimenti
+probs$winner_first_num<-ifelse(probs$winner_first=='no',0,1)
 
+# Rappresento il modello graficamente
+rpart.plot(res.tree$finalModel)
+
+# Mostro grafico sull'importanza delle feature
+plot(caret::varImp(res.tree))
+
+# Determino la confusionMatrix del modello 
+cm <- caret::confusionMatrix(res.tree)
+
+# Determino parametri di qualità
 precision <- cm$table[1]/(cm$table[1]+cm$table[2])
 recall <- cm$table[1]/(cm$table[1]+cm$table[3])
 f <- (2*precision*recall)/(precision+recall)
 
-test_pred <- predict(res, newdata = test)
-cm_pred <- confusionMatrix(as.factor(test_pred), as.factor(test$winner_first_label))
+# Determino la confusionMatrix del modello di previsione
+cm_pred <- confusionMatrix(as.factor(probs$winner_first), as.factor(test$winner_first_label))
 
+# Determino parametri di qualità
 precision_pred <- cm_pred$table[1]/(cm_pred$table[1]+cm_pred$table[2])
 recall_pred <- cm_pred$table[1]/(cm_pred$table[1]+cm_pred$table[3])
 f_pred <- (2*precision_pred*recall_pred)/(precision_pred+recall_pred)
 
-# Rileggo il file con i combattimenti e risultati per fare il training del modello
-real_test<-read.csv('./tests.csv',sep=",",stringsAsFactors=F)
+# Test Reale
 
 # Trova i nomi dei contendenti dati gli ID
 real_test$First_pokemon_name<-sapply(real_test$First_pokemon, function(x) names$Name[match(x, names$id)])
@@ -137,14 +204,17 @@ real_test$Second_pokemon_type<-sapply(real_test$Second_pokemon_name, function(x)
 real_test$First_pokemon_legendary<-sapply(real_test$First_pokemon_name, function(x) pokemon$Legendary[match(x, pokemon$Name)])
 real_test$Second_pokemon_legendary<-sapply(real_test$Second_pokemon_name, function(x) pokemon$Legendary[match(x, pokemon$Name)])
 
+# Determino attributo advantage per la tabella real_test
+real_test$advantage<-mapply(makeAdvantage2, real_test$First_pokemon_type, real_test$Second_pokemon_type)
+
 #scale numerical features
-temp_real_test<- data.frame(real_test %>% dplyr::select(Diff_attack ,Diff_defense, Diff_sp_defense,Diff_sp_attack,Diff_speed ,Diff_HP, First_pokemon_legendary, Second_pokemon_legendary))
+temp_real_test<- data.frame(real_test %>% dplyr::select(Diff_attack ,Diff_defense, Diff_sp_defense,Diff_sp_attack,Diff_speed ,Diff_HP, First_pokemon_legendary, Second_pokemon_legendary, advantage))
 # Determino quali colonne di temp sono attributi di tipo numerico
 ind <- sapply(temp_real_test, is.numeric)
 # temp[ind] contiene solo le colonne di real_test aventi valori di tipo numerico
 temp_real_test[ind] <- lapply(temp_real_test[ind], scale) # Scala le colonne
 
-test_real_pred <- predict(res, newdata = temp_real_test)
+test_real_pred <- predict(res.tree, newdata=temp_real_test, type='prob')
 
 real_test$winner_first_label<-test_real_pred
 
@@ -161,49 +231,18 @@ print(precision_pred)
 print(recall_pred)
 print(f_pred)
 
-# atk<-c('Normal','Fire','Water','Electric','Grass','Ice','Fighting','Poison','Ground','Flying','Psychic','Bug','Rock','Ghost','Dragon','Dark','Steel','Fairy')
-# normal<-c(1,1,1,1,1,1,2,1,1,1,1,1,1,0,1,1,1,1)
-# fire<-c(1,0.5,2,1,0.5,0.5,1,1,2,1,1,0.5,2,1,1,1,0.5,0.5)
-# water<-c(1,0.5,0.5,2,2,0.5,1,1,1,1,1,1,1,1,1,1,0.5,1)
-# elec<-c(1,1,1,0.5,1,1,1,1,2,0.5,1,1,1,1,1,1,0.5,1)
-# grass<-c(1,2,0.5,0.5,0.5,2,1,2,0.5,2,1,2,1,1,1,1,1,1)
-# ice<-c(1,2,1,1,1,0.5,2,1,1,1,1,1,2,1,1,1,2,1)
-# fighting<-c(1,1,1,1,1,1,1,1,1,2,2,0.5,0.5,1,1,0.5,1,2)
-# poison<-c(1,1,1,1,0.5,1,0.5,0.5,2,1,2,0.5,1,1,1,1,1,0.5)
-# ground<-c(1,1,2,0,2,2,1,0.5,1,1,1,1,0.5,1,1,1,1,1)
-# flying<-c(1,1,1,2,0.5,2,0.5,1,0,1,1,0.5,2,1,1,1,1,1)
-# psychic<-c(1,1,1,1,1,1,0.5,1,1,1,0.5,2,1,2,1,2,1,1)
-# bug<-c(1,2,1,1,0.5,1,0.5,1,0.5,2,1,1,2,1,1,1,1,1)
-# rock<-c(0.5,0.5,2,1,2,1,2,0.5,2,0.5,1,1,1,1,1,1,2,1)
-# ghost<-c(0,1,1,1,1,1,0,0.5,1,1,1,0.5,1,2,1,2,1,1)
-# dragon<-c(1,0.5,0.5,0.5,0.5,2,1,1,1,1,1,1,1,1,2,1,1,2)
-# dark<-c(1,1,1,1,1,1,2,1,1,1,0,2,1,0.5,1,0.5,1,2)
-# steel<-c(0.5,2,1,1,0.5,0.5,2,0,2,0.5,0.5,0.5,0.5,1,0.5,1,0.5,0.5)
-# fairy<-c(1,1,1,1,1,1,0.5,2,1,1,1,0.5,1,1,0,0.5,2,1)
-# mytable<-data.frame(Attacking=atk,Normal=normal,Fire=fire,Water=water,Electric=elec,Grass=grass,Ice=ice,Fighting=fighting,Poison=poison,Ground=ground,Flying=flying,Psychic=psychic,Bug=bug,Rock=rock,Ghost=ghost,Dragon=dragon,Dark=dark,Steel=steel,Fairy=fairy)
-# 
-# 
-# combats$First_pokemon_type<-sapply(combats$First_pokemon,function(x) pokemon$Type.1[match(x, pokemon$id)])
-# combats$Second_pokemon_type<-sapply(combats$Second_pokemon,function(x) pokemon$Type.1[match(x, pokemon$id)])
-# combats[2,c('First_pokemon_type','Second_pokemon_type')]
-# 
-# mytable[ which(mytable$Attacking==combats$First_pokemon_type[2]),c(combats$Second_pokemon_type[2])]
-# 
-# makeAdvantage2<-function(type_1,type_2){
-#   val <- mytable[ which(mytable$Attacking==type_1),c(type_2)]
-#   if(val==0){
-#     return('no effect')
-#   }
-#   else if(val==0.5){
-#     return('not too effective')
-#   }
-#   else if(val==1){
-#     return('normal')
-#   }
-#   else if(val==2){
-#     return('effective')
-#   }
-# }
-# combats$advantage<-mapply(makeAdvantage2, combats$First_pokemon_type, combats$Second_pokemon_type)
+# Calcolo Curva ROC
 
+pred<-ROCR::prediction(probs$yes,probs$winner_first_num)
+perf<-ROCR::performance(pred,"tpr", "fpr")
+roc.data <- data.frame(fpr=unlist(perf@x.values),tpr=unlist(perf@y.values),model="tree1")
+auc <- performance(pred, measure = "auc")
+auc <- round(auc@y.values[[1]],3)
+g <- ggplot(roc.data, aes(x=fpr, ymin=0, ymax=tpr)) + 
+  geom_ribbon(alpha=0.2) + geom_line(aes(y=tpr)) + 
+  labs(title= paste0("ROC Curve for Tree with advantage feature w/AUC=", auc),
+       subtitle = "x-axis is False Positive Rate\ny-axis is True Positive rate") + 
+  theme_fivethirtyeight()
+g <- g + geom_segment(x = 0, y = 0, xend = 1, yend = 1, colour = 'red')
+g
 
